9b9800b14c3cf6d18382aafba4de9e9522c71017
sainani8897 <sainani8897@gmail.com>
2022-06-29 22:57:03 +0530

    Update quick-sort.js

--- Sorting/quick-sort.js
+++ Sorting/quick-sort.js
@@ -1,40 +1,50 @@
-var arr = [4,3,33,4,13,1,5,91,21];
-
-
-
-function partionArray(arr){
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
     
-    if(arr.length<2){
-        return arr;
-    }
+console.log("*** Quick Sort ****");
 
-    // console.log(arr);
-    
-    let partion = arr[0];
-    let ita = 0;
-    let left = [];
-    let right = [];
-    let newArr = [];
-    let pivot = arr.shift();
+arr = [12,2,4,53,33,5,1,9,24,11];
 
-    // will divide the array into two parts / based on  the pivot like in 
-    // 1st iteration we will take 4 and create two new arrays which are greater than 4 and less than 4;
-    // ex left  = [3,1] and right = [33,4,13,5,91,21]
-    // so.... again we will call the recursive fuction so and so forth
-    // uncommnet the two console.log for debugging to get a better idea.
-    while(ita < arr.length){
-        if(arr[ita] < partion)
-        {
-            left.push(arr[ita]);
-        }
-        else right.push(arr[ita]);
-        ita++;
-    }
-    
-    //console.log(left,right)
-
-    return newArr.concat(partionArray(left),pivot,partionArray(right));
+function swap(arr,i,j){
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
     
 }
 
-console.log(partionArray(arr));
+function partArr(arr,low,high){
+    
+    let pivot = arr[high];
+    
+    
+    let i = low-1;
+    
+    for(let j=low;j<=high-1;j++){
+        
+        if(arr[j] < pivot){
+            i++
+            swap(arr,i,j);
+        }
+        
+    }
+    swap(arr,i+1,high)
+    
+    return i+1;
+    
+}
+
+function quickSort(arr,low,high){
+    if(low < high){
+        let pi  = partArr(arr,low,high);
+        console.log(pi);
+        
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high)
+    }    
+    
+}
+
+console.log(arr);
+quickSort(arr,0,arr.length-1)
+console.log(arr)
+
